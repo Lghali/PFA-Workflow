db8f7c7f76d3aad448a06f894cbe8cf5c97f4214
Lghali <lghali.ben@gmail.com>
2018-12-21 10:45:31 +0000

    ajoute de class AdminRepository

--- src/main/java/pfa/emsi/dao/AdminRepository.java
+++ src/main/java/pfa/emsi/dao/AdminRepository.java
@@ -1,9 +1,13 @@
 package pfa.emsi.dao;
 
+import java.awt.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pfa.emsi.model.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+	public List findByLogin(String login);
 	
+
 }
