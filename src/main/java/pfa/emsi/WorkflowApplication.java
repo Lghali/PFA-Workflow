c5584c10093426485d561d40bc716c0da664c540
Lghali <lghali.ben@gmail.com>
2018-12-22 06:45:37 +0000

    admin index

--- src/main/java/pfa/emsi/WorkflowApplication.java
+++ src/main/java/pfa/emsi/WorkflowApplication.java
@@ -1,30 +1,23 @@
 package pfa.emsi;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import pfa.emsi.dao.AdminRepository;
-import pfa.emsi.dao.ChefServiceRepository;
-import pfa.emsi.model.Admin;
-import pfa.emsi.model.ChefService;
 
 @SpringBootApplication
 public class WorkflowApplication {
 
 	public static void main(String[] args) throws ParseException {
-		ApplicationContext ctx = SpringApplication.run(WorkflowApplication.class, args);
-		AdminRepository adminRepository = ctx.getBean(AdminRepository.class);
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-		adminRepository.save(new Admin("adresse", df.parse("2018-12-20"), "email", "login", "nom", "numTe", "password", "prenom"));
+//		ApplicationContext ctx = 
+				SpringApplication.run(WorkflowApplication.class, args);
+//		AdminRepository adminRepository = ctx.getBean(AdminRepository.class);
+//		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//		adminRepository.save(new Admin("adresse", df.parse("2018-12-20"), "email", "login", "nom", "numTe", "password", "prenom"));
 		
-		ChefServiceRepository chfserviceRepository = ctx.getBean(ChefServiceRepository.class);
+//		ChefServiceRepository chfserviceRepository = ctx.getBean(ChefServiceRepository.class);
 		
-		chfserviceRepository.save(new ChefService("adresse", df.parse("2018-12-20"), df.parse("2015-04-15"), "email", "login", "nom", "numTe", "password", "prenom"));
+//		chfserviceRepository.save(new ChefService("adresse", df.parse("2018-12-20"), df.parse("2015-04-15"), "email", "login", "nom", "numTe", "password", "prenom"));
 	
 
 	
--- src/main/java/pfa/emsi/web/LoginContoller.java
+++ src/main/java/pfa/emsi/web/LoginContoller.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pfa.emsi.dao.AdminRepository;
+import pfa.emsi.dao.ChefServiceRepository;
+import pfa.emsi.dao.DemandeurRepository;
+import pfa.emsi.dao.DirecteurRepository;
 import pfa.emsi.model.Admin;
+import pfa.emsi.model.ChefService;
+import pfa.emsi.model.Demandeur;
+import pfa.emsi.model.Directeur;
 
 @Controller
 @RequestMapping(value = "/Login")
@@ -16,26 +22,49 @@ public class LoginContoller {
 	@Autowired
 	private AdminRepository adminRepository;
 
-//	@Autowired
-//	private ChefServiceRepository chefServiceRepository;
-//	
-//	@Autowired
-//	private DemandeurRepository demandeurRepository;
-//	
-//	@Autowired
-//	private DirecteurRepository directeurRepository;
+	@Autowired
+	private ChefServiceRepository chefServiceRepository;
+
+	@Autowired
+	private DemandeurRepository demandeurRepository;
+
+	@Autowired
+	private DirecteurRepository directeurRepository;
 
 	@RequestMapping(value = "/Index")
 	public String Index(Model model, @RequestParam(name = "login") String login,
 			@RequestParam(name = "mdp") String mdp) {
 
 		Admin admin = adminRepository.findByLogin(login);
+		ChefService chefService = chefServiceRepository.findByLogin(login);
+		Demandeur demandeur = demandeurRepository.findByLogin(login);
+		Directeur directeur = directeurRepository.findByLogin(login);
 
 		if (admin != null) {
 			if (admin.getPassword().equals(mdp)) {
 				model.addAttribute("user", admin);
-				System.out.println(admin.toString());
-				return "resulta";
+				return "admin_index";
+			} else {
+				return "redirect:/";
+			}
+		} else if (chefService != null) {
+			if (chefService.getPassword().equals(mdp)) {
+				model.addAttribute("user", chefService);
+				return "redirect:/chefService/index";
+			} else {
+				return "redirect:/";
+			}
+		} else if (demandeur != null) {
+			if (demandeur.getPassword().equals(mdp)) {
+				model.addAttribute("user", demandeur);
+				return "redirect:/demandeur/index";
+			} else {
+				return "redirect:/";
+			}
+		} else if (directeur != null) {
+			if (directeur.getPassword().equals(mdp)) {
+				model.addAttribute("user", directeur);
+				return "redirect:/directeur/index";
 			} else {
 				return "redirect:/";
 			}
